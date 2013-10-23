af8de8bc592f5b61455708300a7efdbcf1f7f304
Marshall Allen Beddoe <unmarshal@gmail.com>
2013-10-23 13:05:53 -0700

    d3 example project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3Example::Application.config.secret_key_base = '463482e67f2682516ee33160a2d26715ddc2cf5e3701fd62788c948e3dd9f424ab40cda3c819894ccc15dd2ce1f4267fb0dc96a130068e44bc0626b9ab5c8aee'
